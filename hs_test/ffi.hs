11341fa1e5e44bacb8f341f1d1b3739faa9a3da7
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-10-17 13:25:02 +0200

    Added a safe wrapper around runTable and the TableGen type.

--- hs_test/ffi.hs
+++ hs_test/ffi.hs
@@ -5,6 +5,7 @@ import           Foreign
 import           Foreign.C.String
 import           Foreign.C.Types
 import           Foreign.Ptr
+import           GHC.IO           (unsafePerformIO)
 
 foreign import ccall "wrapper"
   wrap :: (CLong -> CLong) -> IO (FunPtr (CLong -> CLong))
@@ -13,8 +14,17 @@ foreign import ccall "lib.h create_table" rust_create_table :: Ptr CChar -> Int 
 foreign import ccall "lib.h run" rust_run :: Ptr Int -> IO ()
 foreign import ccall "lib.h add_func" rust_add_func :: Ptr Int -> FunPtr a -> FunPtr b -> IO ()
 
-createTable :: String -> IO (Ptr Int)
-createTable msg = newCAStringLen msg >>= uncurry rust_create_table
+data TableGen where
+  TableGen :: (IO (Ptr Int)) -> TableGen
+instance Show TableGen where
+  show :: TableGen -> String
+  show (TableGen ptr) = "TableGen " ++ show (unsafePerformIO ptr)
+
+createTable :: String -> TableGen
+createTable msg = TableGen $ newCAStringLen msg >>= uncurry rust_create_table
+
+runTable :: TableGen -> IO ()
+runTable (TableGen ptr) = ptr >>= rust_run
 
 main :: IO ()
 main = do
